6bc3f8d665864001878fb5fa3f30d7b06066a6e1
Vance Arocho <aroc725@yahoo.com>
2025-08-24 19:20:26 -0400

    Updated POST and DELETE endpoint functionality, added PUT endpoint functionality

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-let todos = [
-    { id: 1, text: 'Learn React' },
-    { id: 2, text: 'Build a server with Express' },
-    { id: 3, text: 'Connect frontend and backend' },
-    ];
+// let todos = [
+//     { id: 1, text: 'Learn React' },
+//     { id: 2, text: 'Build a server with Express' },
+//     { id: 3, text: 'Connect frontend and backend' },
+//     ];
+let todos = [];
 
 app.use(cors());
 app.use(express.json());
@@ -18,23 +19,48 @@ app.get("/api/todos", (req, res) => {
 
 app.post("/api/todos", (req, res) => {
   const todoText = req.body.text;
-  console.log("todoText = " + todoText);
 
-  const ids = todos.map(todo => todo.id);
-  const largestId = Math.max(...ids);
+  let largestId = 0;
+  let ids = [];
+  if (todos.length > 0) {
+    ids = todos.map(todo => todo.id);
+  }
+  if (ids.length > 0) {
+    largestId = Math.max(...ids);
+    if (!largestId || largestId == null) {
+      largestId = 0;
+    }
+  }
 
-  const newTodo = { id: largestId + 1, text: todoText };
-
+  const newTodo = { id: largestId + 1, text: todoText, completed: false };
   todos.push(newTodo);
+
+  res.json(newTodo);
+});
+
+app.delete("/api/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const todo = todos.find(todo => todo.id === todoId);
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+
+  todos = todos.filter(todo => todo.id != todoId);
 
   res.json(todos);
 });
 
-app.delete("/api/todos/:id", (req, res) => {
-  const id = req.params.id;
-  todos = todos.filter(todo => todo.id != id);
+app.put("/api/todos/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const { completed } = req.body;
+  
+  const todo = todos.find(todo => todo.id === todoId);
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
 
-  res.json(todos);
+  todo.completed = completed;
+  res.json(todo);
 });
 
 app.listen(port, () => {
